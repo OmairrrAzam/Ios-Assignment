e740624ed75a5fa91f81c32aa27619585f25a26d
Omair <omairr.azam@gmail.com>
2016-02-22 09:55:04 +0500

    Simple Validation Added

---
+++ assignment/TMProfOrgVC.h
@@ -0,0 +1,13 @@
+//
+//  TMProfOrgVC.h
+//  assignment
+//
+//  Created by Apple on 20/02/2016.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMProfOrgVC : UIViewController
+
+@end
---
+++ assignment/TMProfOrgVC.m
@@ -0,0 +1,37 @@
+//
+//  TMProfOrgVC.m
+//  assignment
+//
+//  Created by Apple on 20/02/2016.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import "TMProfOrgVC.h"
+
+@interface TMProfOrgVC ()
+
+@end
+
+@implementation TMProfOrgVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
